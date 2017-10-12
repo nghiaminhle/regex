b2f0fe23918e4b5d06810af40622f859f6f6b7a4
nghiaminhle <admin@LAP00332s-MacBook-Pro.local>
2017-10-12 21:23:51 +0700

    fix bug empty string and unlimited star

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ def regex_matcher(s, pattern):
     return match(s, '', 0, pattern)
 
 def match(s, prefix_str, p_index, pattern):
-    if s == prefix_str:
-        if p_index == len(pattern):
-            return True
-        else:
-            return False
+    if s == prefix_str and p_index == len(pattern):
+        return True
     if p_index == len(pattern):
         return False
     if pattern[p_index] == '.':
+        if s == prefix_str:
+            return False
         return match(s, prefix_str + s[len(prefix_str)], p_index + 1, pattern)
     elif pattern[p_index] == '*':
+        if s == prefix_str:
+            return match(s, prefix_str, p_index+1, pattern)
+        elif match(s, prefix_str, p_index+1, pattern):
+            return True
         new_prefix = prefix_str
         s_index = len(prefix_str)
         while s_index < len(s):
@@ -22,11 +25,14 @@ def match(s, prefix_str, p_index, pattern):
             if match(s, new_prefix, p_index + 1, pattern):
                 return True
             s_index += 1
-        return False
+        return match(s, new_prefix, p_index + 1, pattern)
     else:
+        if s == prefix_str:
+            return False
         if s[len(prefix_str)] != pattern[p_index]:
             return False
         return match(s, prefix_str + s[len(prefix_str)], p_index + 1, pattern)
+
 
 def main():
     testcases = [
@@ -42,20 +48,20 @@ def main():
         ['abc', '*d', False],
         ['abcd', 'a*d', True],
         ['abcd', 'a***d', True],
-        ['abcd', '*', True]
-        #['', '*', True],
-        #['abc', '*c*', True],
-        #['bedc', '*b*c', True],
-        #['abcd', 'a.c*', True],
-        #['abcd', 'a.c*.', True],
-        #['', '*.', False]
+        ['abcd', '*', True],
+        ['', '*', True],
+        ['abc', '*c*', True],
+        ['bedc', '*b*c', True],
+        ['abcd', 'a.c*', True],
+        ['abcd', 'a.c*.', True],
+        ['', '*.', False]
     ]
 
-    #for case in testcases:
-    #    print('Executing test case %s %s %s' % (case[0], case[1], case[2]))
-    #    assert regex_matcher(case[0], case[1]) == case[2]
-    
-    print(regex_matcher('abcd', 'a**d'))
+    for case in testcases:
+        print('Executing test case %s %s %s' % (case[0], case[1], case[2]))
+        assert regex_matcher(case[0], case[1]) == case[2]
+
+    #print(regex_matcher('abcd', 'a**d'))
 
 
 if __name__ == "__main__":
